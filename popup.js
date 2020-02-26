63fc04042700c88239de733e24eac4e521641c42
JuliusJacobitz <julius@jacobitz.de>
2020-02-26 21:16:32 +0100

    added request api

--- popup.js
+++ popup.js
@@ -1,5 +1,38 @@
-let sourceLanguage = ""
-let targetLanguage = ""
+function YandexRequest(srcL,trgtL,texts) {
+    
+    const request = new XMLHttpRequest()
+    var url = ""
+
+
+    const base = "https://translate.yandex.net/api/v1.5/tr.json/translate"
+    const APIKEY = 'trnsl.1.1.20200226T195431Z.1cd979c15441b1cc.c821796bffa7156971ea828237a0102d64cb5235'
+    var inText = encodeURI(texts)
+    var lan = srcL+"-"+trgtL               //en-ru
+
+
+    url = base+"?key="+APIKEY+"&text="+inText+"&lang="+lan
+
+    
+    request.open('GET',url,true)
+
+    request.onload = function() {
+        
+        let res = this.response
+        let Jres = JSON.parse(res)
+        re = Jres.text[0]
+         
+    
+      }                                             //problem: den response als retrun zurück geben 
+      
+      // Send request
+      request.send()
+
+
+}
+
+
+
+
 document.addEventListener('DOMContentLoaded', function () {
     let TransButton = document.getElementById("TranslateButton")
     TransButton.addEventListener('click',onTranslateClick,false)
@@ -20,12 +53,19 @@ function onTranslateClick() {
     //alert(inputValue)
     let outDiv = document.getElementById("odiv")
     let sATDiv = document.getElementById("sourceAndTarget")
-    let newP = "<p>" + inputValue + "</p>" +"\n" 
+    var sourceLanguages = document.getElementById("SourceLanguage").value
+    var targetLanguages = document.getElementById("TargetLanguage").value
+
+    var ergi = YandexRequest(sourceLanguages,targetLanguages,inputValue)
+
+    let newP = "<p>" + ergi + "</p>" +"\n" 
     outDiv.innerHTML = newP
-    sATDiv.innerHTML =   "\n" + "<p>" + "Source: "+ sourceLanguage +"<br>"+ "Target: "+ targetLanguage + "</p>"
+   
+    sATDiv.innerHTML =   "\n" + "<p>" + "Source: "+ sourceLanguages +"<br>"+ "Target: "+ targetLanguages+ "</p>"
+
+
 
 }
-
 
 
 
